3ac28d0a780f4af9607281be527a2e31321ea855
Prashant singh <112719877+prashant923@users.noreply.github.com>
2025-06-16 05:04:32 +0530

    Tabbar Added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Tabs } from "expo-router";
+import CustomNavBar from "../components/CustomNavBar"; // adjust the path if needed
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
+export default function Layout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Tabs
+      tabBar={(props) => <CustomNavBar {...props} />}
+      screenOptions={{ headerShown: false }} // optional
+    >
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="discover" options={{ title: "Discover" }} />
+      <Tabs.Screen name="saved" options={{ title: "Saved" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    </Tabs>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Gaurav</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'plum'
+    }
+})
